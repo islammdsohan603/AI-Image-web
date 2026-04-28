67b36ee6f9778720f7649587434984671cf62995
islammdsohan603 <islammdsohan603@gmail.com>
2026-04-28 23:11:25 +0600

    proxy error handle

--- src/proxy.js
+++ src/proxy.js
@@ -1,23 +1,29 @@
 import { NextResponse } from 'next/server';
-import { auth } from './app/lib/auth';
+import { auth } from '@/app/lib/auth';
 import { headers } from 'next/headers';
 
-export async function middleware(request) {
+export async function proxy(request) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
+  const pathname = request.nextUrl.pathname;
 
+  // যদি user login না থাকে
   if (!session) {
-    return NextResponse.redirect(new URL('/signup', request.url));
+    // protected routes
+    if (
+      pathname.startsWith('/allphoto') ||
+      pathname.startsWith('/photos') ||
+      pathname.startsWith('/profile')
+    ) {
+      return NextResponse.redirect(new URL('/signup', request.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/allphoto',
-    '/photos/:path*',
-  ],
+  matcher: ['/allphoto/:path*', '/photos/:path*', '/profile/:path*'],
 };
