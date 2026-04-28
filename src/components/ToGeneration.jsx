ae073ef4e073a3cd0c4c1c09be6a2fde1c9c6984
islammdsohan603 <islammdsohan603@gmail.com>
2026-04-28 10:18:33 +0600

    add topgeneration page

---
+++ src/components/ToGeneration.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ToGeneration = async () => {
+  const res = await fetch('');
+
+  return (
+    <div>
+      <h1>Image Slice</h1>
+    </div>
+  );
+};
+
+export default ToGeneration;
